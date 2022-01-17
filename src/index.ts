fdff35e7ea26b38c9aa53414bf436e4644871bfc
Shahram Pourmakhdomi <spourmak@gmail.com>
2022-01-17 10:40:14 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { defaultValueCtx, Editor } from "@milkdown/core";
+import { nord } from "@milkdown/theme-nord";
+import { commonmark } from "@milkdown/preset-commonmark";
+import { emoji } from "@milkdown/plugin-emoji";
+
+import "./style.css";
+
+Editor.make()
+  .config((ctx) => {
+    ctx.set(defaultValueCtx, "# Milkdown :heartpulse: Vanilla");
+  })
+  .use(nord)
+  .use(commonmark)
+  .use(emoji)
+  .create();
